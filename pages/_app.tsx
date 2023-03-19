c5c9bb3abf74f392a628594b829ca732d3e275e5
Øyvind Hatland <oha@brreg.no>
2023-03-19 21:56:21 +0100

    removed boilerplate

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>RSRK.dev</title>
+        <meta name="description" content="Samling av verktøy laget av og for Redningsselskapets frivillige Sjøredningskorps" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/rs-logo-32x32.png" />
+        <link rel="icon" href="/rs-logo-192x192.png" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+    )
 }
